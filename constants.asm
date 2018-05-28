bbf431d22f2d730f80db83ec277f77852673d652
Rangi <remy.oukaour+rangi@gmail.com>
2018-05-28 12:14:00 -0400

    Define Pokémon constants

--- constants.asm
+++ constants.asm
@@ -4,3 +4,5 @@ INCLUDE "macros.asm"
 
 INCLUDE "constants/hardware_constants.asm"
 INCLUDE "constants/gfx_constants.asm"
+
+INCLUDE "constants/pokemon_constants.asm"
---
+++ constants/pokemon_constants.asm
@@ -0,0 +1,293 @@
+; Pokémon IDs
+; indexes for:
+; - TODO
+	const_def
+	const MON_NOTHING     ; 00
+	const MON_FUSHIGIDANE ; 01 BULBASAUR
+	const MON_FUSHIGISOU  ; 02 IVYSAUR
+	const MON_FUSHIGIBANA ; 03 VENUSAUR
+	const MON_HITOKAGE    ; 04 CHARMANDER
+	const MON_LIZARDO     ; 05 CHARMELEON
+	const MON_LIZARDON    ; 06 CHARIZARD
+	const MON_ZENIGAME    ; 07 SQUIRTLE
+	const MON_KAMEIL      ; 08 WARTORTLE
+	const MON_KAMEX       ; 09 BLASTOISE
+	const MON_CATERPIE    ; 0a CATERPIE
+	const MON_TRANSEL     ; 0b METAPOD
+	const MON_BUTTERFREE  ; 0c BUTTERFREE
+	const MON_BEEDLE      ; 0d WEEDLE
+	const MON_COCOON      ; 0e KAKUNA
+	const MON_SPEAR       ; 0f BEEDRILL
+	const MON_POPPO       ; 10 PIDGEY
+	const MON_PIGEON      ; 11 PIDGEOTTO
+	const MON_PIGEOT      ; 12 PIDGEOT
+	const MON_KORATTA     ; 13 RATTATA
+	const MON_RATTA       ; 14 RATICATE
+	const MON_ONISUZUME   ; 15 SPEAROW
+	const MON_ONIDRILL    ; 16 FEAROW
+	const MON_ARBO        ; 17 EKANS
+	const MON_ARBOK       ; 18 ARBOK
+	const MON_PIKACHU     ; 19 PIKACHU
+	const MON_RAICHU      ; 1a RAICHU
+	const MON_SAND        ; 1b SANDSHREW
+	const MON_SANDPAN     ; 1c SANDSLASH
+	const MON_NIDORAN_F   ; 1d NIDORAN_F
+	const MON_NIDORINA    ; 1e NIDORINA
+	const MON_NIDOQUEEN   ; 1f NIDOQUEEN
+	const MON_NIDORAN_M   ; 20 NIDORAN_M
+	const MON_NIDORINO    ; 21 NIDORINO
+	const MON_NIDOKING    ; 22 NIDOKING
+	const MON_PIPPI       ; 23 CLEFAIRY
+	const MON_PIXY        ; 24 CLEFABLE
+	const MON_ROKON       ; 25 VULPIX
+	const MON_KYUKON      ; 26 NINETALES
+	const MON_PURIN       ; 27 JIGGLYPUFF
+	const MON_PUKURIN     ; 28 WIGGLYTUFF
+	const MON_ZUBAT       ; 29 ZUBAT
+	const MON_GOLBAT      ; 2a GOLBAT
+	const MON_NAZONOKUSA  ; 2b ODDISH
+	const MON_KUSAIHANA   ; 2c GLOOM
+	const MON_RUFFRESIA   ; 2d VILEPLUME
+	const MON_PARAS       ; 2e PARAS
+	const MON_PARASECT    ; 2f PARASECT
+	const MON_KONGPANG    ; 30 VENONAT
+	const MON_MORPHON     ; 31 VENOMOTH
+	const MON_DIGDA       ; 32 DIGLETT
+	const MON_DUGTRIO     ; 33 DUGTRIO
+	const MON_NYARTH      ; 34 MEOWTH
+	const MON_PERSIAN     ; 35 PERSIAN
+	const MON_KODUCK      ; 36 PSYDUCK
+	const MON_GOLDUCK     ; 37 GOLDUCK
+	const MON_MANKEY      ; 38 MANKEY
+	const MON_OKORIZARU   ; 39 PRIMEAPE
+	const MON_GARDIE      ; 3a GROWLITHE
+	const MON_WINDIE      ; 3b ARCANINE
+	const MON_NYOROMO     ; 3c POLIWAG
+	const MON_NYOROZO     ; 3d POLIWHIRL
+	const MON_NYOROBON    ; 3e POLIWRATH
+	const MON_CASEY       ; 3f ABRA
+	const MON_YUNGERER    ; 40 KADABRA
+	const MON_FOODIN      ; 41 ALAKAZAM
+	const MON_WANRIKY     ; 42 MACHOP
+	const MON_GORIKY      ; 43 MACHOKE
+	const MON_KAIRIKY     ; 44 MACHAMP
+	const MON_MADATSUBOMI ; 45 BELLSPROUT
+	const MON_UTSUDON     ; 46 WEEPINBELL
+	const MON_UTSUBOT     ; 47 VICTREEBEL
+	const MON_MENOKURAGE  ; 48 TENTACOOL
+	const MON_DOKUKURAGE  ; 49 TENTACRUEL
+	const MON_ISITSUBUTE  ; 4a GEODUDE
+	const MON_GOLONE      ; 4b GRAVELER
+	const MON_GOLONYA     ; 4c GOLEM
+	const MON_PONYTA      ; 4d PONYTA
+	const MON_GALLOP      ; 4e RAPIDASH
+	const MON_YADON       ; 4f SLOWPOKE
+	const MON_YADORAN     ; 50 SLOWBRO
+	const MON_COIL        ; 51 MAGNEMITE
+	const MON_RARECOIL    ; 52 MAGNETON
+	const MON_KAMONEGI    ; 53 FARFETCH_D
+	const MON_DODO        ; 54 DODUO
+	const MON_DODORIO     ; 55 DODRIO
+	const MON_PAWOU       ; 56 SEEL
+	const MON_JUGON       ; 57 DEWGONG
+	const MON_BETBETER    ; 58 GRIMER
+	const MON_BETBETON    ; 59 MUK
+	const MON_SHELLDER    ; 5a SHELLDER
+	const MON_PARSHEN     ; 5b CLOYSTER
+	const MON_GHOS        ; 5c GASTLY
+	const MON_GHOST       ; 5d HAUNTER
+	const MON_GANGAR      ; 5e GENGAR
+	const MON_IWARK       ; 5f ONIX
+	const MON_SLEEPE      ; 60 DROWZEE
+	const MON_SLEEPER     ; 61 HYPNO
+	const MON_CRAB        ; 62 KRABBY
+	const MON_KINGLER     ; 63 KINGLER
+	const MON_BIRIRIDAMA  ; 64 VOLTORB
+	const MON_MARUMINE    ; 65 ELECTRODE
+	const MON_TAMATAMA    ; 66 EXEGGCUTE
+	const MON_NASSY       ; 67 EXEGGUTOR
+	const MON_KARAKARA    ; 68 CUBONE
+	const MON_GARAGARA    ; 69 MAROWAK
+	const MON_SAWAMULAR   ; 6a HITMONLEE
+	const MON_EBIWALAR    ; 6b HITMONCHAN
+	const MON_BERORINGA   ; 6c LICKITUNG
+	const MON_DOGARS      ; 6d KOFFING
+	const MON_MATADOGAS   ; 6e WEEZING
+	const MON_SIHORN      ; 6f RHYHORN
+	const MON_SIDON       ; 70 RHYDON
+	const MON_LUCKY       ; 71 CHANSEY
+	const MON_MONJARA     ; 72 TANGELA
+	const MON_GARURA      ; 73 KANGASKHAN
+	const MON_TATTU       ; 74 HORSEA
+	const MON_SEADRA      ; 75 SEADRA
+	const MON_TOSAKINTO   ; 76 GOLDEEN
+	const MON_AZUMAO      ; 77 SEAKING
+	const MON_HITODEMAN   ; 78 STARYU
+	const MON_STARMIE     ; 79 STARMIE
+	const MON_BARRIERD    ; 7a MR. MIME
+	const MON_STRIKE      ; 7b SCYTHER
+	const MON_ROUGELA     ; 7c JYNX
+	const MON_ELEBOO      ; 7d ELECTABUZZ
+	const MON_BOOBER      ; 7e MAGMAR
+	const MON_KAILIOS     ; 7f PINSIR
+	const MON_KENTAUROS   ; 80 TAUROS
+	const MON_KOIKING     ; 81 MAGIKARP
+	const MON_GYARADOS    ; 82 GYARADOS
+	const MON_LAPLACE     ; 83 LAPRAS
+	const MON_METAMON     ; 84 DITTO
+	const MON_EIEVUI      ; 85 EEVEE
+	const MON_SHOWERS     ; 86 VAPOREON
+	const MON_THUNDERS    ; 87 JOLTEON
+	const MON_BOOSTER     ; 88 FLAREON
+	const MON_PORYGON     ; 89 PORYGON
+	const MON_OMNITE      ; 8a OMANYTE
+	const MON_OMSTAR      ; 8b OMASTAR
+	const MON_KABUTO      ; 8c KABUTO
+	const MON_KABUTOPS    ; 8d KABUTOPS
+	const MON_PTERA       ; 8e AERODACTYL
+	const MON_KABIGON     ; 8f SNORLAX
+	const MON_FREEZER     ; 90 ARTICUNO
+	const MON_THUNDER     ; 91 ZAPDOS
+	const MON_FIRE        ; 92 MOLTRES
+	const MON_MINIRYU     ; 93 DRATINI
+	const MON_HAKURYU     ; 94 DRAGONAIR
+	const MON_KAIRYU      ; 95 DRAGONITE
+	const MON_MEWTWO      ; 96 MEWTWO
+	const MON_MEW         ; 97 MEW
+PROTO_POKEMON EQU const_value
+	const MON_HAPPA       ; 98 CHIKORITA
+	const MON_HANAMOGURA  ; 99 BAYLEEF
+	const MON_HANARYUU    ; 9a MEGANIUM
+	const MON_HONOGUMA    ; 9b
+	const MON_VOLBEAR     ; 9c
+	const MON_DAINABEAR   ; 9d
+	const MON_KURUSU      ; 9e
+	const MON_AQUA        ; 9f
+	const MON_AQUARIA     ; a0
+	const MON_HOHO        ; a1 HOOTHOOT
+	const MON_BOBO        ; a2 NOCTOWL
+	const MON_PACHIMEE    ; a3 MAREEP
+	const MON_MOKOKO      ; a4 FLAAFFY
+	const MON_DENRYU      ; a5 AMPHAROS
+	const MON_MIKON       ; a6
+	const MON_MONJA       ; a7
+	const MON_JARANRA     ; a8 TANGROWTH
+	const MON_HANEEI      ; a9 MANTINE
+	const MON_PUKU        ; aa QWILFISH
+	const MON_SHIBIREFUGU ; ab
+	const MON_PICHU       ; ac PICHU
+	const MON_PY          ; ad CLEFFA
+	const MON_PUPURIN     ; ae IGGLYPUFF
+	const MON_MIZUUO      ; af QUAGSIRE
+	const MON_NATY        ; b0 NATU
+	const MON_NATIO       ; b1 XATU
+	const MON_GYOPIN      ; b2
+	const MON_MARIL       ; b3 MARILL
+	const MON_MANBO1      ; b4
+	const MON_IKARI       ; b5
+	const MON_GROTESS     ; b6
+	const MON_EKSING      ; b7 CROBAT
+	const MON_PARA        ; b8
+	const MON_KOKUMO      ; b9 SPINARAK
+	const MON_TWOHEAD     ; ba ARIADOS
+	const MON_YOROIDORI   ; bb SKARMORY
+	const MON_ANIMON      ; bc
+	const MON_HINAZU      ; bd
+	const MON_SUNNY       ; be SUNFLORA
+	const MON_PAON        ; bf PHANPY
+	const MON_DONPHAN     ; c0 DONPHAN
+	const MON_TWINZ       ; c1
+	const MON_KIRINRIKI   ; c2 GIRAFARIG
+	const MON_PAINTER     ; c3 SMEARGLE
+	const MON_KOUNYA      ; c4
+	const MON_RINRIN      ; c5
+	const MON_BERURUN     ; c6
+	const MON_NYOROTONO   ; c7 POLITOED
+	const MON_YADOKING    ; c8 SLOWKING
+	const MON_ANNON       ; c9 UNOWN
+	const MON_REDIBA      ; ca LEDYBA
+	const MON_MITSUBOSHI  ; cb LEDIAN
+	const MON_PUCHICORN   ; cc
+	const MON_EIFIE       ; cd ESPEON
+	const MON_BLACKY      ; ce UMBREON
+	const MON_TURBAN      ; cf
+	const MON_BETBABY     ; d0
+	const MON_TEPPOUO     ; d1 REMORAID
+	const MON_OKUTANK     ; d2 OCTILLERY
+	const MON_GONGU       ; d3 TYROGUE
+	const MON_KAPOERER    ; d4 HITMONTOP
+	const MON_PUDIE       ; d5
+	const MON_HANEKO      ; d6 HOPPIP
+	const MON_POPONEKO    ; d7 SKIPLOOM
+	const MON_WATANEKO    ; d8 JUMPLUFF
+	const MON_BARIRINA    ; d9 MIME_JR
+	const MON_LIP         ; da SMOOCHUM
+	const MON_ELEBABY     ; db ELEKID
+	const MON_BOOBY       ; dc MAGBY
+	const MON_KIREIHANA   ; dd BELLOSSOM
+	const MON_TSUBOMITTO  ; de
+	const MON_MILTANK     ; df MILTANK
+	const MON_BOMBSEEKER  ; e0
+	const MON_GIFT        ; e1 DELIBIRD
+	const MON_KOTORA      ; e2
+	const MON_RAITORA     ; e3
+	const MON_MADAME      ; e4
+	const MON_NOROWARA    ; e5
+	const MON_KYONPAN     ; e6
+	const MON_YAMIKARASU  ; e7 MURKROW
+	const MON_HAPPI       ; e8 BLISSEY
+	const MON_SCISSORS    ; e9 SCIZOR
+	const MON_PURAKKUSU   ; ea
+	const MON_DEVIL       ; eb HOUNDOUR
+	const MON_HELGAA      ; ec HOUNDOOM
+	const MON_WOLFMAN     ; ed
+	const MON_WARWOLF     ; ee
+	const MON_PORYGON2    ; ef PORYGON2
+	const MON_NAMEIL      ; f0
+	const MON_HAGANEIL    ; f1 STEELIX
+	const MON_KINGDRA     ; f2 KINGDRA
+	const MON_RAI         ; f3 RAIKOU
+	const MON_EN          ; f4 ENTEI
+	const MON_SUI         ; f5 SUICUNE
+	const MON_NYULA       ; f6 SNEASEL
+	const MON_HOUOU       ; f7 HO_OH
+	const MON_TOGEPY      ; f8 TOGEPI
+	const MON_BULU        ; f9 SNUBBULL
+	const MON_TAIL        ; fa AIPOM
+	const MON_LEAFY       ; fb LEAFEON
+NUM_POKEMON EQU const_value + -1
+	const MON_FC          ; fc
+	const MON_FD          ; fd
+	const MON_FE          ; fe
+
+; Annon forms
+; indexes for:
+; - TODO
+	const_def 1
+	const ANNON_A ;  1
+	const ANNON_B ;  2
+	const ANNON_C ;  3
+	const ANNON_D ;  4
+	const ANNON_E ;  5
+	const ANNON_F ;  6
+	const ANNON_G ;  7
+	const ANNON_H ;  8
+	const ANNON_I ;  9
+	const ANNON_J ; 10
+	const ANNON_K ; 11
+	const ANNON_L ; 12
+	const ANNON_M ; 13
+	const ANNON_N ; 14
+	const ANNON_O ; 15
+	const ANNON_P ; 16
+	const ANNON_Q ; 17
+	const ANNON_R ; 18
+	const ANNON_S ; 19
+	const ANNON_T ; 20
+	const ANNON_U ; 21
+	const ANNON_V ; 22
+	const ANNON_W ; 23
+	const ANNON_X ; 24
+	const ANNON_Y ; 25
+	const ANNON_Z ; 26
+NUM_ANNON EQU const_value + -1 ; 26
