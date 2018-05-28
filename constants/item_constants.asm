16324afe6062f4ba5f82f43cb51ff2fd11188f59
Rangi <remy.oukaour+rangi@gmail.com>
2018-05-28 14:56:11 -0400

    Define item constants; declare TM/HM moves

---
+++ constants/item_constants.asm
@@ -0,0 +1,257 @@
+; Item IDs
+	const_def
+	const ITEM_NONE             ; 00
+	const ITEM_MASTER_BALL      ; 01
+	const ITEM_ULTRA_BALL       ; 02
+	const ITEM_03               ; 03 *BRIGHTPOWDER
+	const ITEM_GREAT_BALL       ; 04
+	const ITEM_POKE_BALL        ; 05
+	const ITEM_TOWN_MAP         ; 06
+	const ITEM_BICYCLE          ; 07
+	const ITEM_MOON_STONE       ; 08
+	const ITEM_ANTIDOTE         ; 09
+	const ITEM_BURN_HEAL        ; 0a
+	const ITEM_ICE_HEAL         ; 0b
+	const ITEM_AWAKENING        ; 0c
+	const ITEM_PARLYZ_HEAL      ; 0d
+	const ITEM_FULL_RESTORE     ; 0e
+	const ITEM_MAX_POTION       ; 0f
+	const ITEM_HYPER_POTION     ; 10
+	const ITEM_SUPER_POTION     ; 11
+	const ITEM_POTION           ; 12
+	const ITEM_ESCAPE_ROPE      ; 13
+	const ITEM_REPEL            ; 14
+	const ITEM_MAX_ELIXER       ; 15
+	const ITEM_FIRE_STONE       ; 16
+	const ITEM_THUNDERSTONE     ; 17
+	const ITEM_WATER_STONE      ; 18
+	const ITEM_19               ; 19
+	const ITEM_HP_UP            ; 1a
+	const ITEM_PROTEIN          ; 1b
+	const ITEM_IRON             ; 1c
+	const ITEM_CARBOS           ; 1d
+	const ITEM_1E               ; 1e *LUCKY_PUNCH
+	const ITEM_CALCIUM          ; 1f
+	const ITEM_RARE_CANDY       ; 20
+	const ITEM_X_ACCURACY       ; 21
+	const ITEM_LEAF_STONE       ; 22
+	const ITEM_23               ; 23 *METAL_POWDER
+	const ITEM_NUGGET           ; 24
+	const ITEM_POKE_DOLL        ; 25
+	const ITEM_FULL_HEAL        ; 26
+	const ITEM_REVIVE           ; 27
+	const ITEM_MAX_REVIVE       ; 28
+	const ITEM_GUARD_SPEC       ; 29
+	const ITEM_SUPER_REPEL      ; 2a
+	const ITEM_MAX_REPEL        ; 2b
+	const ITEM_DIRE_HIT         ; 2c
+	const ITEM_2D               ; 2d
+	const ITEM_FRESH_WATER      ; 2e
+	const ITEM_SODA_POP         ; 2f
+	const ITEM_LEMONADE         ; 30
+	const ITEM_X_ATTACK         ; 31
+	const ITEM_32               ; 32
+	const ITEM_X_DEFEND         ; 33
+	const ITEM_X_SPEED          ; 34
+	const ITEM_X_SPECIAL        ; 35
+	const ITEM_COIN_CASE        ; 36
+	const ITEM_ITEMFINDER       ; 37
+	const ITEM_POKE_FLUTE       ; 38
+	const ITEM_EXP_SHARE        ; 39
+	const ITEM_OLD_ROD          ; 3a
+	const ITEM_GOOD_ROD         ; 3b
+	const ITEM_3C               ; 3c *SILVER_LEAF
+	const ITEM_SUPER_ROD        ; 3d
+	const ITEM_PP_UP            ; 3e
+	const ITEM_ETHER            ; 3f
+	const ITEM_MAX_ETHER        ; 40
+	const ITEM_ELIXER           ; 41
+	const ITEM_MYSTIC_PETAL     ; 42 *RED_SCALE
+	const ITEM_WHITE_FEATHER    ; 43 *SECRETPOTION
+	const ITEM_CONFUSE_CLAW     ; 44 *S_S_TICKET
+	const ITEM_WISDOM_ORB       ; 45 *MYSTERY_EGG
+	const ITEM_STEEL_SHELL      ; 46 *CLEAR_BELL
+	const ITEM_UP_GRADE         ; 47 *SILVER_WING
+	const ITEM_STRANGE_THREAD   ; 48 *MOOMOO_MILK
+	const ITEM_BIG_LEAF         ; 49 *QUICK_CLAW
+	const ITEM_QUICK_NEEDLE     ; 4a *PSNCUREBERRY
+	const ITEM_4B               ; 4b *GOLD_LEAF
+	const ITEM_SHARP_STONE      ; 4c *SOFT_SAND
+	const ITEM_BLACK_FEATHER    ; 4d *SHARP_BEAK
+	const ITEM_SHARP_FANG       ; 4e *PRZCUREBERRY
+	const ITEM_SNAKESKIN        ; 4f *BURNT_BERRY
+	const ITEM_ELECTRIC_POUCH   ; 50 *ICE_BERRY
+	const ITEM_TOXIC_NEEDLE     ; 51 *POISON_BARB
+	const ITEM_KINGS_ROCK       ; 52
+	const ITEM_STRANGE_POWER    ; 53 *BITTER_BERRY
+	const ITEM_LIFE_TAG         ; 54 *MINT_BERRY
+	const ITEM_POISON_FANG      ; 55 *RED_APRICORN
+	const ITEM_CORDYCEPS        ; 56 TINYMUSHROOM
+	const ITEM_DRAGON_FANG      ; 57 *BIG_MUSHROOM
+	const ITEM_SILVERPOWDER     ; 58
+	const ITEM_DIGGING_CLAW     ; 59 *BLU_APRICORN
+	const ITEM_5A               ; 5a
+	const ITEM_AMULET_COIN      ; 5b
+	const ITEM_MIGRAINE_SEED    ; 5c *YLW_APRICORN
+	const ITEM_COUNTER_CUFF     ; 5d *GRN_APRICORN
+	const ITEM_TALISMAN_TAG     ; 5e CLEANSE_TAG
+	const ITEM_STRANGE_WATER    ; 5f MYSTIC_WATER
+	const ITEM_TWISTEDSPOON     ; 60
+	const ITEM_ATTACK_NEEDLE    ; 61 *WHT_APRICORN
+	const ITEM_POWER_BRACER     ; 62 BLACKBELT
+	const ITEM_HARD_STONE       ; 63 *BLK_APRICORN
+	const ITEM_64               ; 64
+	const ITEM_JIGGLING_BALLOON ; 65 *PNK_APRICORN
+	const ITEM_FIRE_MANE        ; 66 *BLACKGLASSES
+	const ITEM_SLOWPOKETAIL     ; 67
+	const ITEM_EARTH            ; 68 *PINK_BOW
+	const ITEM_STICK            ; 69
+	const ITEM_FLEE_FEATHER     ; 6a SMOKE_BALL
+	const ITEM_ICE_FANG         ; 6b NEVERMELTICE
+	const ITEM_FOSSIL_SHARD     ; 6c *MAGNET
+	const ITEM_GROSS_GARBAGE    ; 6d *MIRACLEBERRY
+	const ITEM_BIG_PEARL        ; 6e PEARL
+	const ITEM_CHAMPION_BELT    ; 6f *BIG_PEARL
+	const ITEM_TAG              ; 70 *EVERSTONE
+	const ITEM_SPELL_TAG        ; 71
+	const ITEM_5_YEN_COIN       ; 72 *RAGECANDYBAR
+	const ITEM_GUARD_THREAD     ; 73 *GS_BALL
+	const ITEM_STIMULUS_ORB     ; 74 *BLUE_CARD
+	const ITEM_CALM_BERRY       ; 75 *MIRACLE_SEED
+	const ITEM_THICK_CLUB       ; 76
+	const ITEM_FOCUS_ORB        ; 77 FOCUS_BAND
+	const ITEM_78               ; 78
+	const ITEM_DETECT_ORB       ; 79 *ENERGYPOWDER
+	const ITEM_LONG_TONGUE      ; 7a *ENERGY_ROOT
+	const ITEM_LOTTO_TICKET     ; 7b *HEAL_POWDER
+	const ITEM_EVERSTONE        ; 7c *REVIVAL_HERB
+	const ITEM_SHARP_HORN       ; 7d *HARD_STONE
+	const ITEM_LUCKY_EGG        ; 7e
+	const ITEM_LONG_VINE        ; 7f *CARD_KEY
+	const ITEM_MOMS_LOVE        ; 80 *MACHINE_PART
+	const ITEM_SMOKESCREEN      ; 81 *EGG_TICKET
+	const ITEM_WET_HORN         ; 82 *LOST_ITEM
+	const ITEM_SKATEBOARD       ; 83 *STARDUST
+	const ITEM_CRIMSON_JEWEL    ; 84 STAR_PIECE
+	const ITEM_INVISIBLE_WALL   ; 85 *BASEMENT_KEY
+	const ITEM_SHARP_SCYTHE     ; 86 *PASS
+	const ITEM_87               ; 87
+	const ITEM_ICE_BIKINI       ; 88 *
+	const ITEM_THUNDER_FANG     ; 89 *
+	const ITEM_FIRE_CLAW        ; 8a *CHARCOAL
+	const ITEM_TWIN_HORNS       ; 8b *BERRY_JUICE
+	const ITEM_SPIKE            ; 8c *SCOPE_LENS
+	const ITEM_BERRY            ; 8d *
+	const ITEM_APPLE            ; 8e *
+	const ITEM_METAL_COAT       ; 8f
+	const ITEM_PRETTY_TAIL      ; 90 *DRAGON_FANG
+	const ITEM_WATER_TAIL       ; 91 *
+	const ITEM_LEFTOVERS        ; 92
+	const ITEM_ICE_WING         ; 93 *
+	const ITEM_THUNDER_WING     ; 94 *
+	const ITEM_FIRE_WING        ; 95 *
+	const ITEM_96               ; 96 *MYSTERYBERRY
+	const ITEM_DRAGON_SCALE     ; 97
+	const ITEM_BERSERK_GENE     ; 98
+	const ITEM_HEART_STONE      ; 99 *
+	const ITEM_FIRE_TAIL        ; 9a *
+	const ITEM_THUNDER_TAIL     ; 9b *
+	const ITEM_SACRED_ASH       ; 9c
+	const ITEM_TM_HOLDER        ; 9d *HEAVY_BALL
+	const ITEM_MAIL             ; 9e *FLOWER_MAIL
+	const ITEM_BALL_HOLDER      ; 9f *LEVEL_BALL
+	const ITEM_BAG              ; a0 *LURE_BALL
+	const ITEM_IMPORTANT_BAG    ; a1 *FAST_BALL
+	const ITEM_POISON_STONE     ; a2 *
+	const ITEM_A3               ; a3 *LIGHT_BALL
+	const ITEM_A4               ; a4 *FRIEND_BALL
+	const ITEM_A5               ; a5 *MOON_BALL
+	const ITEM_A6               ; a6 *LOVE_BALL
+	const ITEM_A7               ; a7 *NORMAL_BOX
+	const ITEM_A8               ; a8 *GORGEOUS_BOX
+	const ITEM_A9               ; a9 *SUN_STONE
+	const ITEM_AA               ; aa *POLKADOT_BOW
+	const ITEM_AB               ; ab
+	const ITEM_AC               ; ac *UP_GRADE
+	const ITEM_AD               ; ad *BERRY
+	const ITEM_AE               ; ae *GOLD_BERRY
+	const ITEM_AF               ; af *SQUIRTBOTTLE
+	const ITEM_B0               ; b0
+	const ITEM_B1               ; b1 *PARK_BALL
+	const ITEM_B2               ; b2 *RAINBOW_WING
+	const ITEM_B3               ; b3
+	const ITEM_B4               ; b4 *BRICK_PIECE
+	const ITEM_B5               ; b5 *SURF_MAIL
+	const ITEM_B6               ; b6 *LITEBLUEMAIL
+	const ITEM_B7               ; b7 *PORTRAITMAIL
+	const ITEM_B8               ; b8 *LOVELY_MAIL
+	const ITEM_B9               ; b9 *EON_MAIL
+	const ITEM_BA               ; ba *MORPH_MAIL
+	const ITEM_BB               ; bb *BLUESKY_MAIL
+	const ITEM_BC               ; bc *MUSIC_MAIL
+	const ITEM_BD               ; bd *MIRAGE_MAIL
+	const ITEM_BE               ; be
+	const ITEM_BF               ; bf *TM01
+	const ITEM_C0               ; c0 *TM02
+	const ITEM_C1               ; c1 *TM03
+	const ITEM_C2               ; c2 *TM04
+	const ITEM_C3               ; c3
+	const ITEM_TM01             ; c4 *TM05
+	const ITEM_TM02             ; c5 *TM06
+	const ITEM_TM03             ; c6 *TM07
+	const ITEM_TM04             ; c7 *TM08
+	const ITEM_C8               ; c8 *TM09
+	const ITEM_TM05             ; c9 *TM10
+	const ITEM_TM06             ; ca *TM11
+	const ITEM_TM07             ; cb *TM12
+	const ITEM_TM08             ; cc *TM13
+	const ITEM_TM09             ; cd *TM14
+	const ITEM_TM10             ; ce *TM15
+	const ITEM_TM11             ; cf *TM16
+	const ITEM_TM12             ; d0 *TM17
+	const ITEM_TM13             ; d1 *TM18
+	const ITEM_TM14             ; d2 *TM19
+	const ITEM_TM15             ; d3 *TM20
+	const ITEM_TM16             ; d4 *TM21
+	const ITEM_TM17             ; d5 *TM22
+	const ITEM_TM18             ; d6 *TM23
+	const ITEM_TM19             ; d7 *TM24
+	const ITEM_TM20             ; d8 *TM25
+	const ITEM_TM21             ; d9 *TM26
+	const ITEM_TM22             ; da *TM27
+	const ITEM_TM23             ; db *TM28
+	const ITEM_TM24             ; dc *
+	const ITEM_TM25             ; dd *TM29
+	const ITEM_TM26             ; de *TM30
+	const ITEM_TM27             ; df *TM31
+	const ITEM_TM28             ; e0 *TM32
+	const ITEM_E1               ; e1 *TM33
+	const ITEM_TM29             ; e2 *TM34
+	const ITEM_TM30             ; e3 *TM35
+	const ITEM_TM31             ; e4 *TM36
+	const ITEM_TM32             ; e5 *TM37
+	const ITEM_TM33             ; e6 *TM38
+	const ITEM_TM34             ; e7 *TM39
+	const ITEM_TM35             ; e8 *TM40
+	const ITEM_TM36             ; e9 *TM41
+	const ITEM_TM37             ; ea *TM42
+	const ITEM_TM38             ; eb *TM43
+	const ITEM_TM39             ; ec *TM44
+	const ITEM_TM40             ; ed *TM45
+	const ITEM_TM41             ; ee *TM46
+	const ITEM_TM42             ; ef *TM47
+	const ITEM_TM43             ; f0 *TM48
+	const ITEM_TM44             ; f1 *TM49
+	const ITEM_TM45             ; f2 *TM50
+	const ITEM_TM46             ; f3 *HM01
+	const ITEM_TM47             ; f4 *HM02
+	const ITEM_TM48             ; f5 *HM03
+	const ITEM_TM49             ; f6 *HM04
+	const ITEM_TM50             ; f7 *HM05
+	const ITEM_HM01             ; f8 *HM06
+	const ITEM_HM02             ; f9 *HM07
+	const ITEM_HM03             ; fa *
+	const ITEM_HM04             ; fb *
+	const ITEM_HM05             ; fc *
+	const ITEM_HM06             ; fd *
+	const ITEM_HM07             ; fe *
