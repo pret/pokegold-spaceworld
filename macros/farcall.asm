0614ce8b8fbfccfbaf8cda89ed083417c12bcce1
ISSOtm <eldredhabert0@gmail.com>
2018-05-29 22:32:27 +0200

    Fix incorrect function in farcall/callfar

--- macros/farcall.asm
+++ macros/farcall.asm
@@ -2,13 +2,13 @@
 farcall: MACRO ; bank, address
 	ld a, BANK(\1)
 	ld hl, \1
-	call FarCall
+	call FarCall_hl
 ENDM
 
 callfar: MACRO ; address, bank
 	ld hl, \1
 	ld a, BANK(\1)
-	call FarCall
+	call FarCall_hl
 ENDM
 
 homecall: MACRO
