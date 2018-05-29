39c56be66f34870fdcf0730163b13483f72e2c11
Sanqui <gsanky@gmail.com>
2018-05-29 12:34:22 +0200

    Add main menu strings

--- main.asm
+++ main.asm
@@ -5,3 +5,25 @@ INCLUDE "constants.asm"
 SECTION "Font Gfx", ROMX[$4362], BANK[$3e]
     INCBIN "gfx/font.1bpp"
 
+SECTION "Main Menu Definition", ROMX[$5418], BANK[$01]
+MainMenuHeader:
+    db $40
+    db 0, 0, 7, 13
+    dw .data
+    db 1 ; default option
+.data
+    db $80
+    db 0 ; number of options
+
+    dw $5461
+    db $8a, $1f
+    dw MainMenuStrings
+
+MainMenuStrings: ; 01:5428
+    db "つづきから　はじめる@"
+    db "さいしょから　はじめる@"
+    db "せっていを　かえる@"
+    db "#を　あそぶ@"
+    db "じかんセット@"
+; 01:5457
+
