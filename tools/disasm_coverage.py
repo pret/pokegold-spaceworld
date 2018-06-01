3f86bca093910dea3ee0e92e6b533b1b580e2685
Rangi <remy.oukaour+rangi@gmail.com>
2018-05-31 23:53:32 -0400

    Alternate bank colors green and red in coverage.png

--- tools/disasm_coverage.py
+++ tools/disasm_coverage.py
@@ -2,83 +2,77 @@
 # coding: utf-8
 
 from __future__ import division
+
 import os
-import argparse
-from mapreader import MapReader
-import png
-from colorsys import hls_to_rgb
 import sys
+import argparse
+import png
+from mapreader import MapReader
+from colorsys import hls_to_rgb
 
 if __name__ == "__main__":
     # argument parser
     ap = argparse.ArgumentParser()
     ap.add_argument("-o", dest="filename", default="coverage.png")
-    ap.add_argument("-s", dest="statsname", default="coverage.stats")
+    ap.add_argument("-s", dest="statsname", default="coverage.log")
     ap.add_argument("-m", dest="mapfile", required=True)
     ap.add_argument("-b", dest="num_banks", required=True, type=lambda x: int(x, 0))
-    
     args = ap.parse_args()
     
-    width = 256
-    bpp = 8 #bytes per pixel
-    height = (args.num_banks * 0x4000 + (width * bpp - 1)) // (width * bpp)
+    bank_mask = 0x3FFF
+    bank_size = 0x4000 # bytes
+    width = 256 # pixels per row
+    bpp = 8 # bytes per pixel
+    
+    rom_size = args.num_banks * bank_size # bytes
+    height = (args.num_banks * bank_size + (width * bpp - 1)) // (width * bpp) # pixels
+    rows_per_bank = bank_size // (width * bpp)
     
     r = MapReader()
-    f = open(args.mapfile, 'r')
-    l = f.readlines()
-    f.close()
-    
+    with open(args.mapfile, 'r') as f:
+        l = f.readlines()
     r.read_map_data(l)
-    bank_data = r.bank_data
-    hit_data = [[0 for x in range(width)] for y in range(height)]
+    
+    hit_data = [[0] * width for _ in range(height)]
+    default_bank_data = {'sections': [], 'used': 0, 'slack': bank_size}
+    for bank in range(args.num_banks):
+        data = r.bank_data['ROM Bank'].get(bank, default_bank_data)
+        for s in data['sections']:
+            beg = (s['beg'] & bank_mask) + bank * bank_size
+            end = (s['end'] & bank_mask) + bank * bank_size
+            y_beg = beg // (width * bpp)
+            x_beg = (beg % (width * bpp)) // bpp
+            y_end = end // (width * bpp)
+            x_end = (end % (width * bpp)) // bpp
+            #print('beg {0} end {1}: {2}/{3} -- {4}/{5}'.format(beg, end, y_beg, x_beg, y_end, x_end))
+            # special case y_beg/x_beg and y_end/x_end
+            if (y_beg == y_end and x_beg == x_end):
+                hit_data[y_beg][x_beg] += end - beg + 1
+            else:
+                hit_data[y_beg][x_beg] += bpp - ((beg % (width * bpp)) - x_beg * bpp)
+                hit_data[y_end][x_end] += ((end % (width * bpp)) - x_end * bpp + 1)
+            # regular case
+            for y in range(y_beg, y_end + 1):
+                x_line_beg = 0         if y_beg != y else x_beg + 1
+                x_line_end = width - 1 if y_end != y else x_end - 1
+                for x in range(x_line_beg, x_line_end + 1):
+                    hit_data[y][x] += bpp
     
     with open(args.statsname, 'w') as stats:
-        
-        for bank in range(0,args.num_banks):
-            
-            data = bank_data['ROM Bank'].get(bank, {'sections': [], 'used': 0, 'slack': 0x4000})
-            
-            for s in data['sections']:
-                beg = (s['beg'] & 0x3FFF) + bank * 0x4000
-                end = (s['end'] & 0x3FFF) + bank * 0x4000
-                y_beg = beg // (width * bpp)
-                x_beg = (beg % (width * bpp)) // bpp
-                
-                y_end = end // (width * bpp)
-                x_end = (end % (width * bpp)) // bpp
-                
-                #print('beg {0} end {1}: {2}/{3} -- {4}/{5}'.format(beg, end, y_beg, x_beg, y_end, x_end))
-                
-                # special case y_beg/x_beg and y_end/x_end
-                if (y_beg == y_end and x_beg == x_end):
-                    hit_data[y_beg][x_beg] += end - beg + 1
-                    
-                else:
-                    hit_data[y_beg][x_beg] += bpp - ((beg % (width * bpp)) - x_beg * bpp)
-                    hit_data[y_end][x_end] += ((end % (width * bpp)) - x_end * bpp + 1)
-                
-                # regular case
-                for y in range(y_beg, y_end + 1):
-                    
-                    x_line_beg = 0         if y_beg != y else x_beg + 1
-                    x_line_end = width - 1 if y_end != y else x_end - 1
-                    
-                    for x in range(x_line_beg, x_line_end + 1):
-                        
-                        hit_data[y][x] += bpp
+        # TODO: write stats
+        pass
     
-    with open(args.filename, 'wb') as pngfile:
+    png_data = []
+    for i, row in enumerate(hit_data):
+        bank = i // rows_per_bank
+        hue = 0 if bank % 2 else 120
+        row_png_data = ()
+        for col in row:
+            hls = (hue/360.0, 1.0 - (col/bpp * (100 - 15))/100.0, 1.0)
+            rgb = tuple(255 * x for x in hls_to_rgb(*hls))
+            row_png_data += rgb
+        png_data.append(row_png_data)
     
-        w = png.Writer(width, height);
-        
-        pngdata = []
-        for row in hit_data:
-            row_png_data = ()
-            for col in row:
-                
-                rgb = [255 * x for x in hls_to_rgb(120/360, 1.0 - (col/bpp * (100 - 15))/100, 100/100)]
-                row_png_data += tuple(rgb)
-            
-            pngdata.append(row_png_data)
-    
-        w.write(pngfile, pngdata)
+    with open(args.filename, 'wb') as f:
+        w = png.Writer(width, height)
+        w.write(f, png_data)
