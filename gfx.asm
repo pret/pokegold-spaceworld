8122f9a8d62eb970771b946636c4f215669b45ad
ehw <Knuckles500@gmail.com>
2018-06-01 22:18:11 -0400

    Add trainer battle sprites & update gfx.asm

--- gfx.asm
+++ gfx.asm
@@ -145,6 +145,48 @@ INCBIN "gfx/pokedex/cursors.2bpp"
 PokedexSearchGFX::
 INCBIN "gfx/pokedex/search.2bpp"
 
+SECTION "Trainer Battle Sprites", ROMX[$4000], BANK[$12]
+HayatoPic:: INCBIN "gfx/trainer/hayato.pic"
+AkanePic:: INCBIN "gfx/trainer/akane.pic" ; Gen 1 Bug Catcher
+TsukushiPic:: INCBIN "gfx/trainer/tsukushi.pic"
+EnokiPic:: INCBIN "gfx/trainer/enoki.pic"
+OkeraPic:: INCBIN "gfx/trainer/okera.pic" ; Gen 1 Police Female
+MikanPic:: INCBIN "gfx/trainer/mikan.pic"
+BluePic:: INCBIN "gfx/trainer/blue.pic"	; Gen 1 Pokemaniac
+GamaPic:: INCBIN "gfx/trainer/gama.pic" ; Gen 1 Super Nerd
+RivalPic:: INCBIN "gfx/trainer/rival.pic"
+OakPic:: INCBIN "gfx/trainer/oak.pic"
+ProtagonistPic:: INCBIN "/gfx/trainer/protagonist.pic"
+KurtPic:: INCBIN "/gfx/trainer/kurt.pic"
+YoungsterPic:: INCBIN "gfx/trainer/youngster.pic"
+SchoolboyPic:: INCBIN "gfx/trainer/schoolboy.pic"
+FledglingPic:: INCBIN "gfx/trainer/fledgling.pic"
+LassPic:: INCBIN "gfx/trainer/lass.pic"
+ProfessionalMPic:: INCBIN "gfx/trainer/professional_m.pic"
+ProfessionalFPic:: INCBIN "gfx/trainer/professional_f.pic"
+BeautyPic:: INCBIN "gfx/trainer/beauty.pic"
+PokemaniacPic:: INCBIN "gfx/trainer/pokemaniac.pic"
+RocketMPic:: INCBIN "gfx/trainer/rocket_m.pic"
+TeacherMPic:: INCBIN "gfx/trainer/teacher_m.pic"
+TeacherFPic:: INCBIN "gfx/trainer/teacher_f.pic"
+BugCatcherBoyPic:: INCBIN "gfx/trainer/bug_catcher_boy.pic"
+FisherPic:: INCBIN "gfx/trainer/fisher.pic"
+SwimmerMPic:: INCBIN "gfx/trainer/swimmer_m.pic"
+SwimmerFPic:: INCBIN "gfx/trainer/swimmer_f.pic"
+SuperNerdPic:: INCBIN "gfx/trainer/supernerd.pic"
+EngineerPic:: INCBIN "gfx/trainer/engineer.pic"
+GreenPic:: INCBIN "gfx/trainer/green.pic" ; Gen 1 Green
+BikerPic:: INCBIN "gfx/trainer/biker.pic"
+BurglarPic:: INCBIN "gfx/trainer/burglar.pic"
+FirebreatherPic:: INCBIN "gfx/trainer/firebreather.pic"
+JugglerPic:: INCBIN "gfx/trainer/juggler.pic"
+BlackbeltPic:: INCBIN "gfx/trainer/blackbelt.pic"
+SportsmanPic:: INCBIN "gfx/trainer/sportsman.pic"
+MediumPic:: INCBIN "gfx/trainer/medium.pic"
+KimonoGirlPic:: INCBIN "gfx/trainer/kimonogirl.pic"
+TwinsPic:: INCBIN "gfx/trainer/twins.pic"
+
+
 SECTION "Bank 13 Tilesets 0a", ROMX[$4000], BANK[$13]
 Tileset_0a_GFX:
 INCBIN "gfx/tilesets/tileset_0a.2bpp"
@@ -165,6 +207,7 @@ SECTION "PKMN Sprite Bank List", ROMX[$725C], BANK[$14]
 INCLUDE "gfx/pokemon/pkmn_pic_banks.asm"
 
 INCLUDE "gfx/pokemon/pkmn_pics.asm"
+
 
 SECTION "Annon Pic Ptrs and Pics", ROMX[$4d6a], BANK[$1f]
 INCLUDE "gfx/pokemon/annon_pic_ptrs.asm"
