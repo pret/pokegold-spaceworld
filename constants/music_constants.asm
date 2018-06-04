1512ae6d35ebd1207fc65559cb2f8d3dbd2b378c
ehw <Knuckles500@gmail.com>
2018-06-04 00:01:21 -0400

    Add music_constants.asm

---
+++ constants/music_constants.asm
@@ -0,0 +1,18 @@
+; Song IDs
+; Names taken from pokered.
+
+		const_def
+		
+		const MUSIC_NONE
+		const MUSIC_TITLE_SCREEN
+		const MUSIC_ROUTES1
+		const MUSIC_ROUTES2
+		const MUSIC_GYM_LEADER_BATTLE
+		const MUSIC_TRAINER_BATTLE
+		const MUSIC_INTRO_BATTLE
+		const MUSIC_CITIES1
+		const MUSIC_PKMN_HEALED
+		const MUSIC_BIKE_RIDING
+		const MUSIC_MEET_EVIL_TRAINER
+		const MUSIC_DEFEATED_WILD_MON
+		const MUSIC_SAFARI_ZONE
