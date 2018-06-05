e9a8f8a769f77c289a4aa38423a39e00425b4813
pfero <ohpee@loves.dicksinhisan.us>
2018-06-05 23:59:08 +0200

    Finish disassembling the names functions

--- home/items.asm
+++ home/items.asm
@@ -40,7 +40,7 @@ GiveItem::
 ; and copy the item's name to wcf4b.
 ; Return carry on success.
 	ld a, b
-	ld [wce37], a
+	ld [wNamedObjectIndexBuffer], a
 	ld [wCurItem], a
 	ld a, c
 	ld [wItemQuantity], a
@@ -51,95 +51,3 @@ GiveItem::
 	call CopyStringToCD31
 	scf
 	ret
-
-if DEBUG
-SECTION "GetItemName", ROM0[$376F]
-else
-SECTION "GetItemName", ROM0[$3733]
-endc
-
-GetItemName:: ; 376F
-; given an item ID at [wce37], store the name of the item into a string
-; starting at wcd26
-	push hl
-	push bc
-	ld a, [wce37]
-	cp ITEM_HM01_RED
-	jr nc, .machine
-
-	ld [wcb5b], a
-	ld a, ITEM_NAME
-	ld [wNameCategory], a
-	call GetName
-	jr .finish
-
-.machine
-    call GetMachineName
-.finish
-	ld de, wcd26 ; pointer to where item name is stored in RAM
-	pop bc
-	pop hl
-	ret
-
-if DEBUG
-SECTION "GetMachineName", ROM0[$378E]
-else
-SECTION "GetMachineName", ROM0[$3752]
-endc
-
-GetMachineName::
-; copies the name of the TM/HM in [wce37] to wcd26
-	push hl
-	push de
-	push bc
-	ld a, [wce37]
-	push af
-	cp ITEM_TM01_RED
-	jr nc, .WriteTM
-; if HM, then write "HM" and add 5 to the item ID, so we can reuse the
-; TM printing code
-	add 5
-	ld [wce37], a
-	ld hl, HiddenPrefix
-	ld bc, 6
-	jr .WriteMachinePrefix
-.WriteTM
-	ld hl, TechnicalPrefix
-	ld bc, 5
-.WriteMachinePrefix
-	ld de, wcd26
-	call CopyBytes
-; now get the machine number and convert it to text
-	ld a, [wce37]
-	sub ITEM_TM01_RED - 1
-	ld b, "０"
-.FirstDigit
-	sub 10
-	jr c, .SecondDigit
-	inc b
-	jr .FirstDigit
-.SecondDigit
-	add 10
-	push af
-	ld a, b
-	ld [de], a
-	inc de
-	pop af
-	ld b, "０"
-	add b
-	ld [de], a
-	inc de
-	ld a, "@"
-	ld [de], a
-	pop af
-	ld [wce37], a
-	pop bc
-	pop de
-	pop hl
-	ret
-
-TechnicalPrefix:
-	db "わざマシン@"
-
-HiddenPrefix:
-	db "ひでんマシン@"
