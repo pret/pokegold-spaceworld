96097e8c48c332cbf821289141ade48eaa886fcb
obskyr <powpowd@gmail.com>
2018-07-07 07:10:27 +0200

    Build only one ROM

--- home/init.asm
+++ home/init.asm
@@ -5,21 +5,8 @@ SECTION "home/init.asm@Entry point", ROM0
 	jp Init
 
 SECTION "home/init.asm@Global check value", ROM0
-; The ROM has an incorrect global check, so set it here
-; It is not corrected by RGBFIX
-if def(GOLD)
-if DEBUG
-    db $21, $C6
-else
-    db $7e, $49
-endc
-else
-if DEBUG
-    db $c9, $2f
-else
-    db $b1, $7a
-endc
-endc
+	; To work around a bug in rgbfix v0.3.7.
+	dw 0
 
 SECTION "home/init.asm@Init", ROM0
 
