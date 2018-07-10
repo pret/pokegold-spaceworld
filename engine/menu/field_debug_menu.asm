11b62e73cb6f776b1e6c89cb4b9e6805d866ac60
KernelEquinox <cry0@protonmail.ch>
2018-07-10 00:30:02 -0500

    More Field Debug disassembly stuff

--- engine/menu/field_debug_menu.asm
+++ engine/menu/field_debug_menu.asm
@@ -5,17 +5,17 @@ SECTION "Field Debug Menu", ROMX[$4000], BANK[$3F]
 FieldDebugMenuHeader:: ; 3F:4000
 	db MENU_BACKUP_TILES ; flags
 	menu_coords 00, 00, SCREEN_WIDTH - 13, SCREEN_HEIGHT - 1
-	dw .MenuData
+	dw .FieldDebugMenuData
 	db 01 ; default option
 
-.MenuData: ; 3F:4008
+.FieldDebugMenuData: ; 3F:4008
 	db STATICMENU_CURSOR | STATICMENU_WRAP | STATICMENU_ENABLE_START | STATICMENU_ENABLE_LEFT_RIGHT ; flags
 	db 0 ; items
 	dw FieldDebugMenuPages
 	dw PlaceMenuStrings
-	dw .Strings
+	dw .FieldDebugStrings
 
-.Strings: ; 3F:4010
+.FieldDebugStrings: ; 3F:4010
 	db "りセット@"
 	db "とじる@"
 	db "わくせん@"
@@ -47,35 +47,35 @@ FieldDebugMenuHeader:: ; 3F:4000
 	db "じっけん@"
 
 FieldDebugJumpTable:: ; 3F:4094
-	dw ResetPrompt
-	dw CloseFieldDebugMenu
-	dw ChangeWindowBorder
-	dw InitGameSelect
-	dw ChangeTransportation
-	dw ChangeFieldTileset
-	dw ShowTownMap
-	dw CharacterViewer
-	dw NameEntry
-	dw InitToolGear
-	dw ClearSetEventFlags
-	dw RecoverPokemon
-	dw InitCableClub
-	dw NextFieldDebugMenuPage
-	dw NPCMovementTest
-	dw TogglePokemonFollowing
-	dw PlayerFollowNPCTest
-	dw Warp
-	dw FieldCutTest
-	dw CheckTile
-	dw MoveToEntrance
-	dw ToggleNPCMovement
-	dw InitMapViewer
-	dw ItemTest
-	dw AccessPC
-	dw AccessPokeMart
-	dw SetTeleportLocation
-	dw InitVRAMViewer
-	dw OpenPokeGear
+	dw FieldDebug_ResetPrompt
+	dw FieldDebug_CloseFieldDebugMenu
+	dw FieldDebug_ChangeWindowBorder
+	dw FieldDebug_InitGameSelect
+	dw FieldDebug_ChangeTransportation
+	dw FieldDebug_ChangeFieldTileset
+	dw FieldDebug_ShowTownMap
+	dw FieldDebug_CharacterViewer
+	dw FieldDebug_NameEntry
+	dw FieldDebug_InitToolGear
+	dw FieldDebug_ClearSetEventFlags
+	dw FieldDebug_RecoverPokemon
+	dw FieldDebug_InitCableClub
+	dw FieldDebug_NextFieldDebugMenuPage
+	dw FieldDebug_NPCMovementTest
+	dw FieldDebug_TogglePokemonFollowing
+	dw FieldDebug_PlayerFollowNPCTest
+	dw FieldDebug_Warp
+	dw FieldDebug_FieldCutTest
+	dw FieldDebug_CheckTile
+	dw FieldDebug_MoveToEntrance
+	dw FieldDebug_ToggleNPCMovement
+	dw FieldDebug_InitMapViewer
+	dw FieldDebug_ItemTest
+	dw FieldDebug_AccessPC
+	dw FieldDebug_AccessPokeMart
+	dw FieldDebug_SetTeleportLocation
+	dw FieldDebug_InitVRAMViewer
+	dw FieldDebug_OpenPokeGear
 
 RESET_GAME       EQU 0  ; unused
 CLOSE_MENU       EQU 1
@@ -208,7 +208,7 @@ FieldDebugMenu:: ; 3F:40E9
 	ld [hStartmenuCloseAndSelectHookEnable], a
 	jr .Clean
 
-CloseFieldDebugMenu: ; 3F:415A
+FieldDebug_CloseFieldDebugMenu: ; 3F:415A
 	ld a, $02
 	ret
 
@@ -237,7 +237,7 @@ ChangeFieldDebugMenuPage: ; 3F:415D
 	ld [wFieldDebugMenuPage], a
 	jr PlayMenuOpeningSound
 
-NextFieldDebugMenuPage: ; 3F:4181
+FieldDebug_NextFieldDebugMenuPage: ; 3F:4181
 	ld a, [wFieldDebugMenuPage]
 	and a
 	jr z, .set_page_1
@@ -260,4 +260,259 @@ PlayMenuOpeningSound: ; 3F:418F
 	ld a, $00
 	ret
 
-; 3F:41A1
+CloseFieldDebugMenuTextBoxLoop: ; 3F:41A1
+	push bc
+	ld b, a
+.loop
+	call GetJoypad
+	ld a, [hJoyDown]
+	and b
+	jr z, .loop
+	pop bc
+	ret
+
+ShowTextBoxAndExitFieldDebugMenu: ; 3F:41AD
+	call MenuTextBox
+	ld a, $03
+	call CloseFieldDebugMenuTextBox
+	call CloseWindow
+	ret
+
+FieldDebug_ChangeWindowBorder: ; 3F:41B9
+	call ChangeWindowBorderMenu
+	ld a, $00
+	ret
+
+FieldDebug_ResetPrompt: ; 3F:41BF
+	call ResetPromptMenu
+	ld a, $00
+	ret
+
+Unreferenced_ShowTrainerCard ; 3F:41C5
+	ld hl, $5F25 ; 04:5F25 = Trainer Card display subroutine
+	ld a, $04
+	call FarCall_hl
+	ld a, $00
+	ret
+
+FieldDebug_ChangeTransportation: ; 3F:41D0
+	ld hl, ChangeTransportationMenuHeader
+	call LoadMenuHeader
+	ld a, [wPlayerState]
+	call GetCurrentTransportation
+	ld [wMenuCursorBuffer], a
+	dec a
+	call CopyNameFromMenu
+	call VerticalMenu
+	jp c, .exit
+	ld a, [w2DMenuDataEnd]
+	call SetCurrentTransportation
+	ld hl, wPlayerState
+	cp [hl]
+	jr z, .exit
+	cp $04
+	jr z, .check_surf
+	ld [wPlayerState], a
+	and a
+	jr z, .walking
+.biking
+.skateboard
+	ld a, [w2DMenuDataEnd]
+	dec a
+	call CopyNameFromMenu
+	call CloseWindow
+	call CloseWindow
+	ld hl, TransportationPlayerString2
+	call MenuTextBox
+	jr .update_sprite
+.walking
+	ld a, $ff
+	ld [wcc9a], a
+	call CloseWindow
+	call CloseWindow
+	call PlayMapMusic
+	ld hl, TransportationPlayerString1
+	call MenuTextBox
+.update_sprite
+	ld hl, GetPlayerSprite
+	ld a, $05
+	call FarCall_hl
+	ld a, $0f
+	call CloseFieldDebugMenuTextBoxLoop
+	call CloseWindow
+	ld a, $03
+	ret
+.check_surf
+	call CheckIfFacingSurfableTile
+	jr c, .cannot_surf
+	ld [wPlayerState], a
+	call Function_fc2ee
+	ld a, [w2DMenuDataEnd]
+	dec a
+	call CopyNameFromMenu
+	call CloseWindow
+	call CloseWindow
+	ld hl, TransportationPlayerString2
+	call MenuTextBox
+	jr .update_sprite
+.cannot_surf
+	ld hl, TransportationCannotSurfString
+	call MenuTextBox
+	ld a, $0f
+	call CloseFieldDebugMenuTextBoxLoop
+	call CloseWindow
+.exit
+	call CloseWindow
+	ld a, $00
+	ret
+
+ChangeTransportationMenuHeader:: ; 3F:426F
+	db MENU_BACKUP_TILES ; flags
+	menu_coords 03, 03, SCREEN_WIDTH - 8, SCREEN_HEIGHT - 5
+	dw .TransportationMenuData
+	db 01 ; default option
+
+.TransportationMenuData: ; 3F:4277
+	db STATICMENU_CURSOR | STATICMENU_WRAP ; flags
+	db 04 ; items
+	db "あるき@"
+	db "じてんしゃ@"
+	db "スケボー@"
+	db "ラプラス@"
+
+TransportationPlayerString1: ; 3F:428D
+	db "<PLAYER>は@"
+
+TransportationPlayerString2: ; 3F:429D
+	db "<PLAYER>は@"
+
+TransportationCannotSurfString: ; 3F:42AC
+	db "ここでは　のることが"
+	next "できません"
+	prompt
+
+SetCurrentTransportation: ; 3F:42BE
+	ld hl, TransportationOptions
+	dec a
+	ld c, a
+	ld b, $00
+	add hl, bc
+	ld a, [hl]
+	ret
+
+GetCurrentTransportation: ; 3F:42C8
+	ld hl, TransportationOptions
+	ld b, $01
+.loop
+	cp [hl]
+	jr z, .return
+	inc hl
+	inc b
+	jr .loop
+.return
+	ld a, b
+	ret
+
+WALKING    EQU 0
+BICYCLE    EQU 1
+SKATEBOARD EQU 2
+SURFING    EQU 4
+
+TransportationOptions: ; 3F:42D6
+	db WALKING
+	db BICYCLE
+	db SKATEBOARD
+	db SURFING
+
+CheckIfFacingSurfableTile: ; 3F:42DA
+	push af
+	call GetFacingTileCoord
+	and $f0
+	cp $20
+	jr z, .surfable
+	cp $40
+	jr z, .surfable
+.not_surfable
+	pop af
+	scf
+	ret
+.surfable
+	pop af
+	and a
+	ret
+
+SetSurfDirection: ; 3F:42EE
+	ld a, [wPlayerWalking]
+	srl a
+	srl a
+	ld e, a
+	ld d, $00
+	ld hl, SurfDirectionOptions
+	add hl, de
+	ld a, [hl]
+	ld [wcb77], a
+	ret
+
+DOWN  EQU 04
+UP    EQU 05
+LEFT  EQU 06
+RIGHT EQU 07
+
+SurfDirectionOptions: ; 3F:4301
+	db DOWN
+	db UP
+	db LEFT
+	db RIGHT
+
+ChangeWindowBorderMenu: ; 3F:4305
+	ld hl, ChangeWindowBorderMenuHeader
+	call LoadMenuHeader
+	ld a, [wActiveFrame]
+	inc a
+	ld [wMenuCursorBuffer], a
+	call VerticalMenu
+	jr c, .close
+	ld a, [wMenuCursorY]
+	dec a
+	ld [wActiveFrame], a
+	push de
+	ld de, SFX_MENU
+	call PlaySFX
+	pop de
+	call LoadFontExtra
+	call WaitBGMap
+.close
+	call CloseWindow
+	ret
+
+ChangeWindowBorderMenuHeader:: ; 3F:4330
+	db MENU_BACKUP_TILES ; flags
+	menu_coords 00, 00, SCREEN_WIDTH - 10, SCREEN_HEIGHT - 1
+	dw .ChangeBorderMenuData
+	db 01 ; default option
+
+.ChangeBorderMenuData: ; 3F:4338
+	db STATICMENU_CURSOR ; flags
+	db 8 ; items
+	db "１ばんめ@"
+	db "２ばんめ@"
+	db "３ばんめ@"
+	db "４ばんめ@"
+	db "５ばんめ@"
+	db "６ばんめ@"
+	db "７ばんめ@"
+	db "８ばんめ@"
+
+ResetPromptMenu: ; 3F:4362
+	ld hl, .ResetPromptString
+	call MenuTextBox
+	call YesNoBox
+	jp nc, Reset
+	call CloseWindow
+	ret
+
+.ResetPromptString: ; 3F:4372
+	db "ほんとにりセットしますか？"
+	done
+
+; 3F:4381
