8dcd2160e0bc0b82afa9a70a9f352396c1729141
obskyr <powpowd@gmail.com>
2019-02-23 17:16:28 +0100

    Synchronize translations to the correct location

--- tools/sync_translations.sh
+++ tools/sync_translations.sh
@@ -3,18 +3,18 @@
 SPREADSHEET_KEY=1ThSNpQWQjSt_QfWeAuN411otkG8ZxFFA2hhl9TosB34
 SHEETS=(
     # CSV path, GID
-    hack/intro.csv            1521166950
-    hack/PlayersHouse1F.csv   157586730
-    hack/PlayersHouse2F.csv   961494702
-    hack/Route1Gate1F.csv     640558502
-    hack/Route1Gate2F.csv     8926885
-    hack/Route1P1.csv         438381029
-    hack/Route1P2.csv         2013016190
-    hack/SilentHills.csv      2145947014
-    hack/SilentHouse.csv      207731727
-    hack/SilentLabP1.csv      549369982
-    hack/SilentLabP2.csv      1367866430
-    hack/SilentPokecenter.csv 245025873
+    hack/text/intro.csv            1521166950
+    hack/text/PlayersHouse1F.csv   157586730
+    hack/text/PlayersHouse2F.csv   961494702
+    hack/text/Route1Gate1F.csv     640558502
+    hack/text/Route1Gate2F.csv     8926885
+    hack/text/Route1P1.csv         438381029
+    hack/text/Route1P2.csv         2013016190
+    hack/text/SilentHills.csv      2145947014
+    hack/text/SilentHouse.csv      207731727
+    hack/text/SilentLabP1.csv      549369982
+    hack/text/SilentLabP2.csv      1367866430
+    hack/text/SilentPokecenter.csv 245025873
 )
 
 for (( i = 0; i < ${#SHEETS[@]}; i += 2 ))
