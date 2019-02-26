6e2bc25eb54c4e182b5a02e68755d672902b9855
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2019-02-26 15:12:02 -0500

    Disassemble SetTime.

---
+++ engine/menu/set_time.asm
@@ -0,0 +1,83 @@
+INCLUDE "constants.asm"
+
+SECTION "engine/menu/set_time.asm", ROMX
+
+SetTime: ; 36:7ae4
+	ld hl, wStartHour
+	ldh a, [hRTCHours]
+	ld [hli], a
+	ldh a, [hRTCMinutes]
+	ld [hl], a
+	ld hl, Textdbaf4
+	call PrintText
+	ret
+
+Textdbaf4: ; 36:7af4
+	deciram wStartHour, 1, 2
+	text "　じ"
+	line "@"
+	deciram wStartMinute, 1, 2
+	text "　ふん？@"
+	start_asm
+.loop
+	call ClearJoypad
+	call GetJoypad
+	ldh a, [hJoyState]
+	and a
+	jr z, .loop
+	bit 0, a
+	jp nz, .sub_dbb63
+	bit 6, a
+	jr nz, .sub_dbb2a
+	bit 7, a
+	jr nz, .sub_dbb37
+	bit 4, a
+	jr nz, .sub_dbb43
+	bit 5, a
+	jr nz, .sub_dbb50
+	jr .loop
+.sub_dbb2a
+	ld a, [wStartHour]
+	cp $17
+	jr z, .sub_dbb5a
+	inc a
+	ld [wStartHour], a
+	jr .sub_dbb5a
+.sub_dbb37
+	ld a, [wStartHour]
+	and a
+	jr z, .sub_dbb5a
+	dec a
+	ld [wStartHour], a
+	jr .sub_dbb5a
+.sub_dbb43
+	ld a, [wStartMinute]
+	cp $3b
+	jr z, .sub_dbb5a
+	inc a
+	ld [wStartMinute], a
+	jr .sub_dbb5a
+.sub_dbb50
+	ld a, [wStartMinute]
+	and a
+	jr z, .sub_dbb5a
+	dec a
+	ld [wStartMinute], a
+.sub_dbb5a
+	ld hl, Textdbaf4 ; meant Textdbb6c?
+	call PrintText
+	jp .loop
+.sub_dbb63
+	call Function04ac
+	call Function0502
+	jp Function3036
+
+Textdbb6c: ; 36:7b6c
+	deciram wStartHour, 1, 2
+	text "　じ"
+	line "@"
+	deciram wStartMinute, 1, 2
+	text "　ふん？"
+	done
+
+; 36:7b98
