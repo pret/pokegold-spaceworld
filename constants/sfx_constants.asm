e77fb41e3a503abb6535c7f7ab98bae05822785d
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2021-01-19 13:37:43 +0700

    Identify some more SFX

--- constants/sfx_constants.asm
+++ constants/sfx_constants.asm
@@ -9,7 +9,19 @@ SFX_ESCAPE_ROPE                 EQU $0B ; player shrink
 SFX_WRONG                       EQU $14
 SFX_SWITCH_POKEMON              EQU $1B
 SFX_POKEFLUTE                   EQU $21
+SFX_POKEDEX_REGISTRATION        EQU $27
 SFX_TITLE_ENTRANCE              EQU $2D
 SFX_PAY_DAY                     EQU $30
 SFX_GAME_FREAK_LOGO_RG          EQU $31
 
+
+; R/G/B/Y fanfares
+SFX_GET_ITEM_RG                 EQU $63
+SFX_UNUSED_FANFARE_1            EQU $64
+SFX_POKEDEX_EVALUATION_RG       EQU $65
+SFX_LEVEL_UP_RG                 EQU $66
+SFX_EVOLUTION_COMPLETE_RG       EQU $67
+SFX_GET_KEY_ITEM_RG             EQU $68
+SFX_UNUSED_FANFARE_2            EQU $69
+SFX_CAUGHT_POKEMON_RG           EQU $6A
+SFX_UNUSED_FANFARE_3            EQU $6B
