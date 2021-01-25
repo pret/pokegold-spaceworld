946f1e02baaefe3438541d3ac0cb21f7999dbbee
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2021-01-25 17:41:17 +0700

    Disassemble subgame menu
    Also adds shims for the minigames in bank $38.

--- engine/menu/debug_menu.asm
+++ engine/menu/debug_menu.asm
@@ -230,3 +230,75 @@ _DebugMenuSoundTest::
 	done
 
 INCLUDE "data/sound_test_text_pointers.inc"
+
+SECTION "engine/menu/debug_menu.asm@Subgame Menu", ROMX
+
+CallSubGameMenu:
+	call ClearTileMap
+	call LoadFont
+	call LoadFontsBattleExtra
+	call ClearSprites
+	call GetMemSGBLayout
+	ld hl, .MenuHeader
+	call CopyMenuHeader
+	call VerticalMenu
+	ret c
+	ld a, [wMenuCursorY]
+	dec a
+	ld e, a
+	ld d, 0
+	ld hl, .Jumptable
+	add hl, de
+	add hl, de
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	ld de, .return
+	push de
+	jp hl
+
+.return
+	jr CallSubGameMenu
+
+.Jumptable:
+	dw SubGameMenu_PokerGame
+	dw SubGameMenu_PuzzleGame
+	dw SubGameMenu_CardFlipGame
+	dw SubGameMenu_PicrossGame
+	dw SubGameMenu_SlotMachineGame
+
+.MenuHeader:
+	db   0 ; flags
+	menu_coords 5, 4, SCREEN_WIDTH - 7, SCREEN_HEIGHT - 3
+	dw .MenuData
+	db   1 ; default option
+
+.MenuData:
+	db $A0
+	db   5 ; items
+	db "ポーカー@"
+	db "１５パズル@"
+	db "しんけい@"
+	db "ピクロス@"
+	db "スロット@"
+
+SubGameMenu_PokerGame:
+	callab PokerMinigame
+	ret
+
+SubGameMenu_PuzzleGame:
+	callab FifteenPuzzleMinigame
+	ret
+
+SubGameMenu_CardFlipGame:
+	callab MemoryMinigame
+	ret
+
+SubGameMenu_PicrossGame:
+	callab PicrossMinigame
+	ret
+
+SubGameMenu_SlotMachineGame:
+	callab SlotMachineGame
+	ret
+
