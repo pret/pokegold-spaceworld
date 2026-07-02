c65ed8f6a3aabf91bbd5e2dcb7aa5d8ef3402f55
Alieneer <37123222+BreavyTF2@users.noreply.github.com>
2026-07-02 11:11:14 -0400

    Update constants/event_constants.asm
    Co-authored-by: Rangi <35663410+Rangi42@users.noreply.github.com>

--- constants/event_constants.asm
+++ constants/event_constants.asm
@@ -2,24 +2,21 @@
 
 ; Silent Hill events
 	const_def
-	const PLAYER_HOUSE_2F_READ_EMAIL ; wd41a
+	const PLAYER_HOUSE_2F_READ_EMAIL
 	const_skip 2
 	const PLAYER_HOUSE_2F_TALKED_TO_KEN
 	const_skip 1
 	const SILENT_HILL_RIVAL_EVENT
 	const SILENT_HILL_HOUSE_READ_RIVAL_EMAIL
 	const SILENT_HILL_TALKED_TO_BLUE
-	const_skip 1                     ; wd41b
+	const_skip 1
 	const SILENT_HILL_LAB_BACK_FOLLOWED_OAK
 	const SILENT_HILL_LAB_BACK_CHOSE_STARTER
-	const_skip 5
-	const_skip 4                     ; wd41c
+	const_skip 9
 	const SILENT_HILL_LAB_FRONT_GOT_POKEDEX
-	const_skip 3
-	const_skip 2                     ; wd41d
-	const SILENT_HILL_LAB_FRONT_RIVAL_BATTLED
 	const_skip 5
-	const_skip 2                     ; wd41e
+	const SILENT_HILL_LAB_FRONT_RIVAL_BATTLED
+	const_skip 7
 	const SILENT_HILL_HOUSE_GOT_POKEGEAR_MAP
 	const_skip 2
 	const SILENT_HILL_HOUSE_KEN_LEFT
