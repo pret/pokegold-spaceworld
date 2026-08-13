df8c8776728d737a562e80927870165ba07e3ee7
Alieneer <37123222+BreavyTF2@users.noreply.github.com>
2026-08-13 18:55:47 -0400

    Name unnamed held Item labels.

--- constants/item_data_constants.asm
+++ constants/item_data_constants.asm
@@ -59,7 +59,7 @@ DEF MAIL_STRUCT_LENGTH EQU MAIL_LINE_LENGTH + 1 + MAIL_LINE_LENGTH + 1 + PLAYER_
 	const HELD_BERRY
 	const HELD_FULL_RESTORE
 	const HELD_LEFTOVERS
-	const HELD_4
+	const HELD_LIFE_TAG
 	const HELD_REVIVE
 	const HELD_RESTORE_PP
 	const HELD_RESTORE_ALL_PP
@@ -94,15 +94,15 @@ DEF MAIL_STRUCT_LENGTH EQU MAIL_LINE_LENGTH + 1 + MAIL_LINE_LENGTH + 1 + PLAYER_
 	const HELD_38
 
 	const_next 40
-	const HELD_40
-	const HELD_41
-	const HELD_METAL_POWDER
-	const HELD_43
-	const HELD_44
-	const HELD_45
-	const HELD_46
-	const HELD_47
-	const HELD_48
+	const HELD_40                ; unreferenced corresponds to max hp
+	const HELD_BASEATTACK_UP     ; raise attack by 10
+	const HELD_BASEDEFENSE_UP    ; raise defense by 10
+	const HELD_BASESPEED_UP      ; raise speed by 10
+	const HELD_BASESP_ATTACK_UP  ; unreferenced, raise special attack by 10
+	const HELD_BASESP_DEFENSE_UP ; raise special defense by 10
+	const HELD_46                ; unreferenced, corresponds to accuracy
+	const HELD_47                ; unreferenced, corresponds to evasion
+	const HELD_BASESTATS_UP      ; raise all stats by 5 - 10
 
 	const_next 50
 	const HELD_NORMAL_BOOST
@@ -125,12 +125,12 @@ DEF MAIL_STRUCT_LENGTH EQU MAIL_LINE_LENGTH + 1 + MAIL_LINE_LENGTH + 1 + PLAYER_
 
 	const_next 70
 	const HELD_CATCH_CHANCE
-	const HELD_71
+	const HELD_FLEE_FEATHER
 	const HELD_ESCAPE
 	const HELD_CRITICAL_UP
 	const HELD_QUICK_CLAW
 	const HELD_FLINCH
 	const HELD_AMULET_COIN
 	const HELD_STRANGE_THREAD
-	const HELD_78
+	const HELD_CONFUSE_CLAW
 	const HELD_FOCUS_ORB
--- data/battle/held_consumables.asm
+++ data/battle/held_consumables.asm
@@ -20,7 +20,7 @@ ConsumableEffects:
 	db HELD_EVASION_UP
 	db HELD_38
 
-	db HELD_71
+	db HELD_FLEE_FEATHER
 	db HELD_ESCAPE
 	db HELD_CRITICAL_UP
 	db -1
